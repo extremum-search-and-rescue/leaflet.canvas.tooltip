20e51607ec4e367cf04390bc7e07c1676483e9df
Alexey Belkin <alexey.belkin@mail.ru>
2023-03-28 07:07:28 +0300

    reduce flickering in tooltip

--- leaflet.canvas.tooltip.ts
+++ leaflet.canvas.tooltip.ts
@@ -15,7 +15,7 @@ namespace L {
             timeout: number = 5000;
             position: ControlPosition = 'absolute';
         }
-        export interface CanvasTooltipEvent {
+        export interface CanvasTooltipEvent extends L.LeafletMouseEvent {
             message: string
             sourceTarget: any;
             timeout: number;
@@ -42,14 +42,15 @@ namespace L {
                 map._controlContainer.appendChild(this._container);
                 L.DomEvent.disableClickPropagation(this._container);
                 this._map.on("gis:tooltip", this.show, this);
-       
                 return this._container;
             }
 
             show(this: CanvasTooltip, event: CanvasTooltipEvent) {
                 if (!event.sourceTarget) return;
                 var elem = this._container;
-                elem.innerHTML = event.message;
+                if (!event.message)
+                    debugger;
+                elem.innerHTML = event.message.replace(/(?:\r\n|\r|\n)/g, '<br>');
                 const messageClass = event.class;
                 if (event.sourceTarget) {
                     if (this._prevTarget) {
@@ -66,12 +67,11 @@ namespace L {
 
                 L.DomEvent.on(this._map.getContainer(), 'mousemove',this._moveWithCursor, this);
                 delete this._container.style.transform;
-                elem.style.top = event.y + 16 + "px";
-                elem.style.left = event.x + 24 + "px";
-                elem.style.display = 'block';
+                this._setTooltipPosition(event.x, event.y);
+                elem.style.visibility = 'visible';
 
                 this.timeoutID = setTimeout(function() {
-                    elem.style.display = 'none';
+                    elem.style.visibility = 'hidden';
                     elem.classList.remove(messageClass);
                 },
                     event.timeout || this.options.timeout);
@@ -80,14 +80,11 @@ namespace L {
                 if (typeof this.timeoutID == 'number') {
                     clearTimeout(this.timeoutID);
                 }
-                this._container.style.display = 'none';
+                this._container.style.visibility = 'hidden';
                 L.DomEvent.off(this._map.getContainer(), 'mousemove',this._moveWithCursor, this);
             }
-            _moveWithCursor(this: CanvasTooltip, e: MouseEvent) {
+            private _setTooltipPosition(x: number, y: number) {
                 const size = this._container.getBoundingClientRect();
-
-                let y = e.clientY;
-                let x = e.clientX;
                 let xModifier = 0;
                 let yModifier = 8;
                 let width = size.width;
@@ -98,17 +95,16 @@ namespace L {
                     //too wide to fit screen to the right from cursor
                     if ((x - width) < 0) {
                         //too wide to fit screen to the left from cursor
-                        if ((x + width) > window.innerWidth) { 
+                        if ((x + width) > window.innerWidth) {
 
                             if ((width + 64) > window.innerHeight) {
                                 // too wide to fit screen, cut width to fit
                                 width = window.innerWidth - 64;
                                 // start from left side
                                 x = 32;
-                            }                     
-                            else
-                            {
-                                x = (window.innerWidth - width)/2;
+                            }
+                            else {
+                                x = (window.innerWidth - width) / 2;
                                 width = window.innerWidth - 64;
                             }
                         }
@@ -117,24 +113,27 @@ namespace L {
                         x = x - width;
                     }
                 }
-                else if(x<width/2)
-                {
+                else if (x < width / 2) {
                     //cursor too left on screen, display tooltip to the right
                     xModifier = 8;
                 }
                 else {
                     //place tooltip centered to bottom of cursor
-                    x = x - width/2;
+                    x = x - width / 2;
                     yModifier = 24;
                 }
-                if(window.innerHeight<(y+yModifier+height+16))
-                {
+                if (window.innerHeight < (y + yModifier + height + 16)) {
                     //tooltip too tall, move it above cursor
                     yModifier = -32;
                 }
-                this._container.style.top = `${y + yModifier}`;
-                this._container.style.left = `${x + xModifier}`;
-                this._container.style.maxWidth = `${width}px`;
+                this._container.style.top = `${y + yModifier}px`;
+                this._container.style.left = `${x + xModifier}px`;
+                this._container.style.maxWidth = `${window.innerWidth - 64}px`;
+            }
+            private _moveWithCursor(this: CanvasTooltip, e: MouseEvent) {
+                let y = e.clientY;
+                let x = e.clientX;
+                this._setTooltipPosition(x, y);
             }
         }
     }
